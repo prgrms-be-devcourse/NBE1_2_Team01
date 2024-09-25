b986f36b681df85382ec2fc34546dc101bced115
junho <>
2024-09-25 22:14:45 +0900

    refactor: #7 클래스 분리 및 커스텀 예외 클래스 생성

--- src/main/java/org/team1/nbe1_2_team01/domain/board/service/response/BoardResponse.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/board/service/response/BoardResponse.java
@@ -1,14 +1,11 @@
 package org.team1.nbe1_2_team01.domain.board.service.response;
 
-import com.querydsl.core.Tuple;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.team1.nbe1_2_team01.domain.board.entity.Board;
-import org.team1.nbe1_2_team01.domain.user.entity.User;
+import org.team1.nbe1_2_team01.domain.board.service.converter.DateTimeToStringConverter;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter @ToString
 public class BoardResponse {
@@ -18,22 +15,36 @@ public class BoardResponse {
     private String writer;
     private String categoryName;
     private String createdAt;
-    private long commentCount;
+    private Long commentCount;
 
-    public BoardResponse(Long id, String title, String writer, LocalDateTime createdAt, long commentCount) {
+    @Builder
+    private BoardResponse(Long id,
+                          String title,
+                          String writer,
+                          String categoryName,
+                          LocalDateTime createdAt,
+                          Long commentCount) {
         this.id = id;
         this.title = title;
         this.writer = writer;
-        this.createdAt = formatTimeToString(createdAt);
+        this.categoryName = categoryName;
+        this.createdAt = DateTimeToStringConverter.convert(createdAt);
         this.commentCount = commentCount;
     }
 
-    public BoardResponse(Long id, String title, String writer, String categoryName, LocalDateTime createdAt, long commentCount) {
-        this(id, title, writer, createdAt, commentCount);
-        this.categoryName = categoryName;
-    }
-
-    private static String formatTimeToString(LocalDateTime createdAt) {
-        return createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    public static BoardResponse of(Long id,
+                                   String title,
+                                   String writer,
+                                   String categoryName,
+                                   LocalDateTime createdAt,
+                                   Long commentCount) {
+        return BoardResponse.builder()
+                .id(id)
+                .title(title)
+                .writer(writer)
+                .categoryName(categoryName)
+                .createdAt(createdAt)
+                .commentCount(commentCount)
+                .build();
     }
 }
