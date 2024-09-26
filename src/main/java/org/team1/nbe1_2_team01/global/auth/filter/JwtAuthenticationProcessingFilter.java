6c912e13b5ce3dcaf066d8a48874404be47283a7
jmd5314 <jmd5314@gmail.com>
2024-09-26 10:57:31 +0900

    feat: #5 jwt 인증 필터 구현

---
+++ src/main/java/org/team1/nbe1_2_team01/global/auth/filter/JwtAuthenticationProcessingFilter.java
@@ -0,0 +1,128 @@
+package org.team1.nbe1_2_team01.global.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.team1.nbe1_2_team01.domain.user.entity.User;
+import org.team1.nbe1_2_team01.domain.user.repository.UserRepository;
+import org.team1.nbe1_2_team01.global.auth.jwt.JwtService;
+
+import java.io.IOException;
+
+/**
+ * Jwt 인증 필터
+ * "/login 이외의 URI 요청을 처리하는 필터
+ * 1. RefreshToken이 없고, AccessToken이 유효한 경우 -> 인증 성공
+ * 2. RefreshToken이 없고, AccessToken이 없거나 유효X 인 경우 -> 인증 실패
+ * 3. RefreshToken이 있는 경우 -> RefreshToken과 비교하여 일치하면 AccessToken 재발급 RefreshToken 재발급
+ */
+@RequiredArgsConstructor
+public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+    private static final String NO_CHECK_URL = "/login";
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 로그인 요청 들어오면 다음 필터 호출
+        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // RefreshToken 추출 없거나 유효하지 않으면 null
+        String refreshToken = jwtService.extractRefreshToken(request)
+                .filter(jwtService::isTokenValid)
+                .orElse(null);
+        if(refreshToken !=null){
+            checkAccessTokenAndAuthentication(request, response, filterChain);
+        }
+    }
+    /**
+     *  리프레시 토큰으로 유저 정보 찾기 & 액세스 토큰/리프레시 토큰 재발급 메소드
+     *  파라미터로 들어온 헤더에서 추출한 리프레시 토큰으로 DB에서 유저를 찾고, 해당 유저가 있다면
+     *  JwtService.createAccessToken()으로 AccessToken 생성,
+     *  reIssueRefreshToken()로 리프레시 토큰 재발급 & DB에 리프레시 토큰 업데이트 메소드 호출
+     *  그 후 JwtService.sendAccessTokenAndRefreshToken()으로 응답 헤더에 보내기
+     */
+    public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
+        userRepository.findByRefreshToken(refreshToken)
+                .ifPresent(user -> {
+                    String reIssuedRefreshToken = reIssueRefreshToken(user);
+                    jwtService.sendAccessAndRefreshToken(response, jwtService.createAccessToken(user.getEmail()),
+                            reIssuedRefreshToken);
+                });
+    }
+
+    /**
+     * 리프레시 토큰 재발급 & DB에 리프레시 토큰 업데이트 메소드
+     * jwtService.createRefreshToken()으로 리프레시 토큰 재발급 후
+     * DB에 재발급한 리프레시 토큰 업데이트 후 Flush
+     */
+    private String reIssueRefreshToken(User user) {
+        String reIssuedRefreshToken = jwtService.createRefreshToken();
+        user.updateRefreshToken(reIssuedRefreshToken);
+        userRepository.saveAndFlush(user);
+        return reIssuedRefreshToken;
+    }
+
+    /**
+     * 액세스 토큰 체크 & 인증 처리 메소드
+     * request에서 extractAccessToken()으로 액세스 토큰 추출 후, isTokenValid()로 유효한 토큰인지 검증
+     * 유효한 토큰이면, 액세스 토큰에서 extractUsername로 Username을 추출한 후 findByUsername()로 해당 유저네임을 사용하는 유저 객체 반환
+     * 그 유저 객체를 saveAuthentication()으로 인증 처리하여
+     * 인증 허가 처리된 객체를 SecurityContextHolder에 담기
+     * 그 후 다음 인증 필터로 진행
+     */
+    public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                                  FilterChain filterChain) throws ServletException, IOException {
+        jwtService.extractAccessToken(request)
+                .filter(jwtService::isTokenValid)
+                .ifPresent(accessToken -> jwtService.extractUsername(accessToken)
+                        .ifPresent(username -> userRepository.findByUsername(username)
+                                .ifPresent(this::saveAuthentication)));
+
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * 인증 허가 메소드
+     * 파라미터의 유저 : 우리가 만든 회원 객체 / 빌더의 유저 : UserDetails의 User 객체
+     *
+     * new UsernamePasswordAuthenticationToken()로 인증 객체인 Authentication 객체 생성
+     * UsernamePasswordAuthenticationToken의 파라미터
+     * 1. 위에서 만든 UserDetailsUser 객체 (유저 정보)
+     * 2. credential(보통 비밀번호로, 인증 시에는 보통 null로 제거)
+     * 3. Collection < ? extends GrantedAuthority>로,
+     * UserDetails의 User 객체 안에 Set<GrantedAuthority> authorities이 있어서 getter로 호출한 후에,
+     * new NullAuthoritiesMapper()로 GrantedAuthoritiesMapper 객체를 생성하고 mapAuthorities()에 담기
+     * SecurityContextHolder.getContext()로 SecurityContext를 꺼낸 후,
+     * setAuthentication()을 이용하여 위에서 만든 Authentication 객체에 대한 인증 허가 처리
+     */
+    public void saveAuthentication(User user) {
+        String password = user.getPassword();
+
+        UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(password)
+                .roles(user.getRole().name())
+                .build();
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetailsUser, null,
+                        authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
