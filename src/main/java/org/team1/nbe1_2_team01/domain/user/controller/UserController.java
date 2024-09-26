465aefef44228adf64f4a17108352619c7b2670a
jmd5314 <jmd5314@gmail.com>
2024-09-26 17:17:23 +0900

    feat: #5 회원가입 제약사항 추가

--- src/main/java/org/team1/nbe1_2_team01/domain/user/controller/UserController.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.team1.nbe1_2_team01.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Long> signUp(@RequestBody UserSignUpDto userSignUpDto){
+    public ResponseEntity<Long> signUp(@Valid @RequestBody UserSignUpDto userSignUpDto){
         Long id = userService.signUp(userSignUpDto);
         return ResponseEntity.ok().body(id);
     }
