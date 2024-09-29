f56c3b46f3238fd5b8cddbe1db4b6a5d8985aa82
junho <>
2024-09-29 23:42:15 +0900

    chore: #17 불필요한 import문 제거

--- src/main/java/org/team1/nbe1_2_team01/domain/board/controller/dto/BoardDeleteRequest.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/board/controller/dto/BoardDeleteRequest.java
@@ -1,9 +1,8 @@
 package org.team1.nbe1_2_team01.domain.board.controller.dto;
 
-
-import lombok.*;
-
-
-public record BoardDeleteRequest(Long boardId, boolean isNotice) {
+public record BoardDeleteRequest(
+        Long boardId,
+        boolean isNotice
+) {
 
 }
