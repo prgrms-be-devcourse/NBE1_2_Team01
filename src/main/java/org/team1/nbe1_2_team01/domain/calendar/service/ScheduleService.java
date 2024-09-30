a4ef934d9a31df7199bf96b2bc446f4a22e4e9a4
sangcci <pshsh910@gmail.com>
2024-09-30 16:53:46 +0900

    refactor: 권한 검증 메서드 분기

--- src/main/java/org/team1/nbe1_2_team01/domain/calendar/service/ScheduleService.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/calendar/service/ScheduleService.java
@@ -23,21 +23,12 @@ public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final BelongingRepository belongingRepository;
 
-    /**
-     * 일정 등록
-     * @param currentUserId 현재 요청 유저 id
-     * @param belongingId 현재 접속된 소속 id
-     * @param addScheduleCommand 등록할 일정 데이터
-     * @return 등록된 일정 id
-     */
     public Long registSchedule(
             Long currentUserId,
             Long belongingId,
             AddScheduleCommand addScheduleCommand
     ) {
-        Belonging belonging = belongingRepository.findById(belongingId).orElseThrow();
-        validateBelonging(currentUserId, belonging);
-        validateOwner(belonging);
+        validatePermission(currentUserId, belongingId);
 
         Calendar calendar = calendarRepository.findByBelongingId(belongingId).orElseThrow();
 
@@ -46,20 +37,12 @@ public class ScheduleService {
         return savedSchedule.getId();
     }
 
-    /**
-     * 일정 수정
-     * @param currentUserId 현재 요청 유저 id
-     * @param belongingId 현재 접속된 소속 id
-     * @param updateScheduleCommand 수정 일정 데이터
-     */
     public void updateSchedule(
             Long currentUserId,
             Long belongingId,
             UpdateScheduleCommand updateScheduleCommand
     ) {
-        Belonging belonging = belongingRepository.findById(belongingId).orElseThrow();
-        validateBelonging(currentUserId, belonging);
-        validateOwner(belonging);
+        validatePermission(currentUserId, belongingId);
 
         Schedule schedule = scheduleRepository.findById(updateScheduleCommand.id())
                 .orElseThrow(() -> new NoSuchElementException("일정을 찾을 수 없습니다."));
@@ -67,20 +50,12 @@ public class ScheduleService {
         schedule.update(updateScheduleCommand);
     }
 
-    /**
-     * 일정 삭제
-     * @param currentUserId 현재 요청 유저 id
-     * @param belongingId 현재 접속된 소속 id
-     * @param scheduleId 삭제할 일정 id
-     */
     public void deleteSchedule(
             Long currentUserId,
             Long belongingId,
             Long scheduleId
     ) {
-        Belonging belonging = belongingRepository.findById(belongingId).orElseThrow();
-        validateBelonging(currentUserId, belonging);
-        validateOwner(belonging);
+        validatePermission(currentUserId, belongingId);
 
         Schedule schedule = scheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new NoSuchElementException("일정을 찾을 수 없습니다."));
@@ -88,12 +63,16 @@ public class ScheduleService {
         scheduleRepository.delete(schedule);
     }
 
-    /**
-     * 임시 검증 메서드 - 소속이 아니면 접근 불가
-     * Belonging Entity에 구현 예정
-     */
+    // 타 도메인 검증 메서드 - 소속이 아니면 접근 불가
+    private void validatePermission(Long currentUserId, Long belongingId) {
+        Belonging belonging = belongingRepository.findById(belongingId).orElseThrow();
+
+        validateBelonging(currentUserId, belonging);
+        validateOwner(belonging);
+    }
+
     private void validateBelonging(Long currentUserId, Belonging belonging) {
-        if (belonging.getUser().getId() != currentUserId) {
+        if (!belonging.getUser().getId().equals(currentUserId)) {
             throw new AccessDeniedException("접근할 수 없습니다.");
         }
     }
