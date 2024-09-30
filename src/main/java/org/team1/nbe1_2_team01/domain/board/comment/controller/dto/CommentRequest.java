13f03ebd813ba5129a59e4beba78432d386c068d
junho <>
2024-09-30 15:57:15 +0900

    fix: #17 Request 객체에 Validation 적용

--- src/main/java/org/team1/nbe1_2_team01/domain/board/comment/controller/dto/CommentRequest.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/board/comment/controller/dto/CommentRequest.java
@@ -2,28 +2,20 @@ package org.team1.nbe1_2_team01.domain.board.comment.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.ToString;
+import jakarta.validation.constraints.Positive;
 import org.team1.nbe1_2_team01.domain.board.entity.Board;
 import org.team1.nbe1_2_team01.domain.board.entity.Comment;
 import org.team1.nbe1_2_team01.domain.user.entity.User;
 
-@Getter @ToString
-public class CommentRequest {
+public record CommentRequest(
+        @NotNull(message = "필수 파라미터가 누락되었습니다.")
+        @Positive(message = "요청 파라미터의 형식이 잘못되었습니다.")
+        Long boardId,
 
-    @NotNull
-    private final Long boardId;
-
-    @NotNull(message = "내용을 입력해주세요.")
-    @NotBlank(message = "내용을 입력해주세요.")
-    private final String content;
-
-    public CommentRequest(Long boardId, String content) {
-        this.boardId = boardId;
-        this.content = content;
-    }
-
+        @NotNull(message = "내용을 입력해주세요.")
+        @NotBlank(message = "내용을 입력해주세요.")
+        String content
+) {
     public Comment toEntity(User user, Board board) {
         return Comment.builder()
                 .user(user)
