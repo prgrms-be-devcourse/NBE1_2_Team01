96343f008d9261d649eeaf2f293bb24e83946eac
sangcci <pshsh910@gmail.com>
2024-10-01 12:43:03 +0900

    refactor: service에 누락된 ErrorCode 적용

--- src/main/java/org/team1/nbe1_2_team01/domain/calendar/service/CalendarService.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/calendar/service/CalendarService.java
@@ -1,12 +1,14 @@
 package org.team1.nbe1_2_team01.domain.calendar.service;
 
+import static org.team1.nbe1_2_team01.global.util.ErrorCode.USER_NOT_OWNER;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.team1.nbe1_2_team01.domain.attendance.exception.AccessDeniedException;
 import org.team1.nbe1_2_team01.domain.calendar.entity.Calendar;
 import org.team1.nbe1_2_team01.domain.calendar.repository.CalendarRepository;
 import org.team1.nbe1_2_team01.domain.group.entity.Belonging;
 import org.team1.nbe1_2_team01.domain.group.repository.BelongingRepository;
+import org.team1.nbe1_2_team01.global.exception.AppException;
 
 @Service
 @RequiredArgsConstructor
@@ -37,7 +39,7 @@ public class CalendarService {
     // 타 도메인 검증 메서드 - 소속이 아니면 접근 불가
     private void validateOwner(Belonging belonging) {
         if (!belonging.isOwner()) {
-            throw new AccessDeniedException("팀원은 수행할 수 없습니다.");
+            throw new AppException(USER_NOT_OWNER);
         }
     }
 }
