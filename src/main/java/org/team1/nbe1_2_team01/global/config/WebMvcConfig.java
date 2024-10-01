9b0357333049343db6a1a615ab649db14ec9d28e
junho <>
2024-10-01 12:30:43 +0900

    refactor: #28 github page 사이트 추가**(secret.yaml 파일 내용 추가 필요)**

--- src/main/java/org/team1/nbe1_2_team01/global/config/WebMvcConfig.java
+++ src/main/java/org/team1/nbe1_2_team01/global/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package org.team1.nbe1_2_team01.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${github.pages.url}")
+    private String githubPagesUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", githubPagesUrl)
                 .allowedMethods("*")
                 .allowCredentials(true);
-
     }
+
 }
