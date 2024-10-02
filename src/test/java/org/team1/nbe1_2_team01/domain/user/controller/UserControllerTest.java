2e1594f5beda2f1528cf5253cd524d4286c81547
jmd5314 <jmd5314@gmail.com>
2024-10-02 10:21:40 +0900

    feat: #20 컨트롤러 테스트 추가 구현

--- src/test/java/org/team1/nbe1_2_team01/domain/user/controller/UserControllerTest.java
+++ src/test/java/org/team1/nbe1_2_team01/domain/user/controller/UserControllerTest.java
@@ -1,43 +1,48 @@
 package org.team1.nbe1_2_team01.domain.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.team1.nbe1_2_team01.domain.user.controller.request.UserSignUpRequest;
+import org.team1.nbe1_2_team01.domain.user.controller.request.UserUpdateRequest;
+import org.team1.nbe1_2_team01.domain.user.entity.Role;
+import org.team1.nbe1_2_team01.domain.user.entity.User;
+import org.team1.nbe1_2_team01.domain.user.repository.UserRepository;
 import org.team1.nbe1_2_team01.domain.user.service.UserService;
-import org.team1.nbe1_2_team01.domain.user.service.response.UserIdResponse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private UserService userService;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
-    private WebApplicationContext context;
+    private PasswordEncoder passwordEncoder;
 
-    @BeforeEach
-    void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
     void 회원가입_성공() throws Exception {
@@ -47,11 +52,6 @@ class UserControllerTest {
                 "1234abcd",
                 "user@gmail.com",
                 "김철수");
-        UserIdResponse response = new UserIdResponse(1L);
-
-        //mocking 된 객체의 특정 메서드 호출시 return_value를 반환하도록 설정
-        when(userService.signUp(any(UserSignUpRequest.class))).thenReturn(response);
-
         // 응답 검증
         mockMvc.perform(post("/api/user/sign-up")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -79,7 +79,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_아이디는_영문과숫자여야한다() throws Exception{
+    void 회원가입_실패_아이디는_영문과숫자여야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "가나다라마바",
                 "1234abcd",
@@ -95,7 +95,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_비밀번호는_8자리이상이어야한다() throws Exception{
+    void 회원가입_실패_비밀번호는_8자리이상이어야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "userA",
                 "1234abc",
@@ -111,7 +111,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_비밀번호는_영문을_포함해야한다() throws Exception{
+    void 회원가입_실패_비밀번호는_영문을_포함해야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "userA",
                 "12341234",
@@ -127,7 +127,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_비밀번호는_숫자를_포함해야한다() throws Exception{
+    void 회원가입_실패_비밀번호는_숫자를_포함해야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "userA",
                 "abcdabcd",
@@ -143,7 +143,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_이메일은_이메일형식이어야한다() throws Exception{
+    void 회원가입_실패_이메일은_이메일형식이어야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "userA",
                 "1234abcd",
@@ -159,7 +159,7 @@ class UserControllerTest {
     }
 
     @Test
-    void 회원가입_실패_이름은_2글자이상이어야한다() throws Exception{
+    void 회원가입_실패_이름은_2글자이상이어야한다() throws Exception {
         UserSignUpRequest request = new UserSignUpRequest(
                 "userA",
                 "1234abcd",
@@ -172,5 +172,77 @@ class UserControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value(400))
                 .andExpect(jsonPath("$.message.name").value("이름은 2자 이상이어야 합니다."));
+    }
+
+    @Test
+    @WithMockUser(username = "userA", roles = {"USER"})
+    void 회원수정_성공() throws Exception {
+
+        //가짜 사용자를 쓰더라도 DB에 사용자가 없으면 현재 로그인된 사용자를 찾는 과정에서 에러가 발생하기때문에 DB에 넣어준다
+        User user = User.builder()
+                .username("userA")
+                .password("1234abcd")
+                .email("user@gmail.com")
+                .name("김철수")
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        UserUpdateRequest request = new UserUpdateRequest(
+                "userB",
+                "abcd1234"
+        );
+
+        mockMvc.perform(patch("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value("Success"))
+                .andExpect(jsonPath("$.result.id").value(1L));
+
+        User updateuser = userRepository.findByUsername("userA")
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자 없음"));
+
+        assertThat(updateuser.getName()).isEqualTo(request.name());
+        assertThat(passwordEncoder.matches(request.password(), updateuser.getPassword())).isTrue();
+    }
+
+    @Test
+    @WithMockUser(username = "userA")
+    void 회원정보조회_성공() throws Exception {
+        
+        User user = User.builder()
+                .username("userA")
+                .password("1234abcd")
+                .email("user@gmail.com")
+                .name("김철수")
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        mockMvc.perform(get("/api/user"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value("Success"))
+                .andExpect(jsonPath("$.result.id").value(1L))
+                .andExpect(jsonPath("$.result.username").value("userA"))
+                .andExpect(jsonPath("$.result.email").value("user@gmail.com"))
+                .andExpect(jsonPath("$.result.name").value("김철수"));
+    }
+
+    @Test
+    @WithMockUser(username = "userA", roles = {"USER"})
+    void 전체사용자조회_사용자권한_접근실패() throws Exception {
+        mockMvc.perform(get("/api/user/admin/all"))
+                .andExpect(status().isForbidden());
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void 전체사용자조회_사용자권한_접근성공() throws Exception {
+        mockMvc.perform(get("/api/user/admin/all"))
+                .andExpect(status().isOk());
     }
 }
