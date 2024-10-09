33b384065fc7b9fa9ba6c362bb01d5b924c1c7d6
sangcci <pshsh910@gmail.com>
2024-10-10 00:01:23 +0900

    refactor: ERD 변경으로 인한 Schedule test 전면 개편
    - Team, Course로 구분해서 테스트 작성
    - GroupAuthServiceTest 제거

--- src/test/java/org/team1/nbe1_2_team01/domain/calendar/fixture/ScheduleRequestFixture.java
+++ src/test/java/org/team1/nbe1_2_team01/domain/calendar/fixture/ScheduleRequestFixture.java
@@ -3,15 +3,14 @@ package org.team1.nbe1_2_team01.domain.calendar.fixture;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import org.team1.nbe1_2_team01.domain.calendar.controller.dto.ScheduleCreateRequest;
 import org.team1.nbe1_2_team01.domain.calendar.entity.ScheduleType;
-import org.team1.nbe1_2_team01.domain.calendar.entity.TeamSchedule;
-import org.team1.nbe1_2_team01.domain.group.entity.Team;
 
-public final class ScheduleFixture {
+public final class ScheduleRequestFixture {
 
-    public static TeamSchedule create_TEAM_SCHEDULE_RBF(Team team) {
-        return TeamSchedule.builder()
-                .team(team)
+    // team schedule
+    public static ScheduleCreateRequest create_TEAM_SCHEDULE_CREATE_REQUEST() {
+        return ScheduleCreateRequest.builder()
                 .name("1차 RBF")
                 .scheduleType(ScheduleType.RBF)
                 .startAt(LocalDateTime.of(LocalDate.now(), LocalTime.of(18, 0, 0)))
