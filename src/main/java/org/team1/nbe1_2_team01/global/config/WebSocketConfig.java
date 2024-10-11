a9bb2bf4f10d88b02e4f28b08da90cabf1b39c44
jmd5314 <jmd5314@gmail.com>
2024-10-11 22:37:41 +0900

    refactor: #67 구조 변경

--- src/main/java/org/team1/nbe1_2_team01/global/config/WebSocketConfig.java
+++ src/main/java/org/team1/nbe1_2_team01/global/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package org.team1.nbe1_2_team01.global;
+package org.team1.nbe1_2_team01.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
