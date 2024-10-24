f0f1b48163da070ba389d36e2461550adb623bf4
power-minu <mw0677@naver.com>
2024-10-24 16:18:46 +0900

    develop 병합 전 작업

--- src/main/java/org/team1/nbe1_2_team01/domain/chat/entity/ParticipantPK.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/chat/entity/ParticipantPK.java
@@ -1,2 +1,33 @@
-package org.team1.nbe1_2_team01.domain.chat.entity;public class ParticipantPK {
+package org.team1.nbe1_2_team01.domain.chat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+public class ParticipantPK implements Serializable {
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "channel_id")
+    private Long channelId;
+
+    public ParticipantPK() {
+    }
+
+    public ParticipantPK(Long userId, Long channelId) {
+        this.userId = userId;
+        this.channelId = channelId;
+    }
+
+    @Override
+    public String toString() {
+        return "ParticipantPK{" +
+                "userId=" + userId +
+                ", channelId=" + channelId +
+                '}';
+    }
 }
--- src/main/java/org/team1/nbe1_2_team01/domain/group/entity/TeamType.java
+++ src/main/java/org/team1/nbe1_2_team01/domain/group/entity/TeamType.java
@@ -1,2 +1,6 @@
-package org.team1.nbe1_2_team01.domain.group.entity;public enum TeamType {
+package org.team1.nbe1_2_team01.domain.group.entity;
+
+public enum TeamType {
+    PROJECT,
+    STUDY
 }
