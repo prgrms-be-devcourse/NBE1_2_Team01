9dbace0111c928bf12fc90f1d22087553e39f4b6
power-minu <mw0677@naver.com>
2024-10-24 15:54:47 +0900

    develop 병합 전 main브랜치 작업

--- src/main/java/org/team1/nbe1_2_team01/domain/attendance/entity/AttendanceIssueType.java
+++
@@ -1,6 +0,0 @@
-package org.team1.nbe1_2_team01.domain.attendance.entity;
-
-public enum AttendanceIssueType {
-    LATE,
-    ABSENT
-}
--- src/main/java/org/team1/nbe1_2_team01/domain/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package org.team1.nbe1_2_team01.domain.user.entity;
-
-public enum Role {
-    ADMIN,
-    USER
-}
